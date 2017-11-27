c5ea2b087f8877b6fcee45482ca2f2f934f450f6
Michael Beck <github@b3ck.me>
2017-11-26 23:34:07 -0700

    fixed some configs and setup the settings form

--- src/settings.js
+++ src/settings.js
@@ -1,30 +1,12 @@
-import {inject, Lazy} from 'aurelia-framework';
-import {HttpClient} from 'aurelia-fetch-client';
-
-// polyfill fetch client conditionally
-const fetch = !self.fetch ? System.import('isomorphic-fetch') : Promise.resolve(self.fetch);
-
-@inject(Lazy.of(HttpClient))
 export class Settings {
-    heading = 'Github Users';
-    users = [];
+    heading = 'Settings';
+    sensorOneName = 'Sensor 1';
+    sensorTwoName = 'Sensor 2';
+    sensorOneTarget = 0;
+    sensorTwoTarget = 0;
 
-    constructor(getHttpClient) {
-        this.getHttpClient = getHttpClient;
-    }
 
-    async activate() {
-        // ensure fetch is polyfilled before we create the http client
-        await fetch;
-        const http = this.http = this.getHttpClient();
-
-        http.configure(config => {
-            config
-                .useStandardConfiguration()
-                .withBaseUrl('https://api.github.com/');
-        });
-
-        const response = await http.fetch('users');
-        this.users = await response.json();
+    saveSettings(form) {
+        console.log(form);
     }
 }
